1ac1b323746e383235c0a97ff9d044f5aaeec3f9
sgolyala87 <sgolyala87@gmail.com>
2024-12-14 19:51:20 +0000

    ProjectInitializationandframeworksetup

---
+++ TestProject/Features/Login.feature
@@ -0,0 +1,22 @@
+﻿Feature: Login
+As a user I want to log into the app to access my account
+
+
+Scenario: Id_01 Successful login with valid credentials
+	Given I navigate to login page
+	When I enter valid credentials
+	And I click on Submit button
+	Then I should redirected to the dashboard
+
+
+Scenario Outline: Id_02 Failed Login with invalid credentials
+	Given I navigate to login page
+	When I enter invalid <Username>  <Password>
+	And I click on Submit button
+	Then I should see an <Errormessage>
+
+Examples:
+	| Username      | Password          | Errormessage              |
+	| incorrectUser | Password123       | Your username is invalid! |
+	| student       | incorrectpassword | Your password is invalid! |
+
---
+++ TestProject/Setup/ScenarioHooks.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace TestProject.Setup
+{
+    [Binding]
+    public class ScenarioHooks
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private IBrowser _browser;
+        private IPage _page;
+        private Config _config;
+
+        public ScenarioHooks(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _config = ConfigReader.GetConfig();
+        }
+
+        [BeforeScenario]
+        public async Task BeforeScenario()
+        {
+            var playwright = await Playwright.CreateAsync();
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            var context = await _browser.NewContextAsync();
+            _page = await context.NewPageAsync();
+
+            
+            _scenarioContext["Browser"] = _browser;
+            _scenarioContext["Page"] = _page;
+        }
+
+        [AfterScenario]
+        public async Task AfterScenario()
+        {
+            if (_browser != null)
+            {
+                await _browser.CloseAsync();
+            }
+        }
+    }
+}
