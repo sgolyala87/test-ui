3987dd40d9cbf84bf1e05bdceae7183a9b04d0cc
sgolyala87 <sgolyala87@gmail.com>
2024-12-16 18:54:37 +0000

    Added logging

---
+++ TestProject/Setup/Logging/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit.Abstractions;
+
+namespace TestProject.Setup.Logging
+{
+    public static class ConsoleLogger
+    {
+        private static readonly List<string> _logs = new();
+
+        public static void LogMessage(string message)
+        {
+            _logs.Add(message);
+        }
+
+        public static void OutputLogs(ITestOutputHelper testOutputHelper)
+        {
+            testOutputHelper.WriteLine("--------Browser console logs-----------");
+            foreach (var log in _logs)
+            {
+                testOutputHelper.WriteLine(log);
+            }
+            _logs.Clear();
+        }
+
+        public static List<string> GetAllLogs() => new(_logs);
+    }
+}
